d81bec7f2fc7c2ac42c3de3f4b29d2753af5bc6a
“Erin <erinkelley27@gmail.com>
2018-07-11 11:10:11 -0400

    added pseudocode

---
+++ main.js
@@ -0,0 +1,20 @@
+// Submit a word
+
+// Create blank spaces - quantity to equal length of the string
+
+// Split the string into an array (like the palindrome)
+
+// Create blank spaces - quantity to equal length of the array - map?
+
+// Submit a guess letter
+
+// Check if guess letter matches any letters in the array
+
+// If yes, letter appears appears in the same index of the blank space array, replacing the space
+
+// If no, body part appears - head, body, left arm, right arm, left leg, right leg
+// Change the color from aqua to pink for the appropriate CSS grid child
+
+// If all blank spaces are replaced with letters, create an alert winner
+
+// If all body parts have appeared, create an alert loser
