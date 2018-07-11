4763307cc4d3de0f4f3a4c6a5d0dc4a07936c804
“Erin <erinkelley27@gmail.com>
2018-07-11 14:57:38 -0400

    blanksArray code, input not working

--- main.js
+++ main.js
@@ -3,29 +3,48 @@ let submitWordButton = document.querySelector('#submit-word-button')
 let word = document.querySelector('#word-input-field').value
 
 // Split the string into an array
-let wordArray = word.split(' ')
+let wordArray = word.split('')
 
-// Create blank spaces - quantity to equal length of the array (map?)
+// Create blanks array
+let blanksArray = []
+
+// Create blank spaces in the blanks - quantity to equal length of the word array
 submitWordButton.addEventListener('click', function (evt) {
   evt.preventDefault()
-  let blanksArray = []
   for (let i = 0; i <= wordArray.length; i++) {
     blanksArray.push('_')
-    // Return them to the blanks spaces div
-    let blankSpaces = document.createElement('p')
-    document.blank-spaces.appenChild(blankSpaces)
+  }
+  // Add the blanks spaces into the blankSpaces div
+//   let blanks = document.createElement('div')
+//   blanksArray = getElementsByClassName('blanksArray')
+//   blanksArray[0].blank - spaces.appendChild(blanks)
+//   blanks.id = 'blanks'
+
+//   for (let i = 0; i < blanksArray.length; i++) {
+//     blanks.appendChild(blanksArray[0])
   }
 })
 
 // Submit a guess letter
+// let submitLetterButton = document.querySelector('#submit-letter-button')
+// let letter = document.querySelector('#letter-input-field').value
 
-// Check if guess letter matches any letters in the array
+// // Check if guess letter matches any letters in the array
+// for (let i = 0; i < wordArray.length; i++) {
+//     if letter === wordArray[i]
+// // If yes, replace the space with the letter
+//     return true
+// }
+// // If no, body part appears - head, body, left arm, right arm, left leg, right leg
+// // Change the color from aqua to pink for the appropriate CSS grid child
+// else {
+//     return false
+// }
 
-// If yes, letter appears appears in the same index of the blank space array, replacing the space
+// // Determine if player has won or lost
+// function callGame () {
+// // If all blank spaces are replaced with letters, create an alert winner
 
-// If no, body part appears - head, body, left arm, right arm, left leg, right leg
-// Change the color from aqua to pink for the appropriate CSS grid child
-
-// If all blank spaces are replaced with letters, create an alert winner
-
-// If all body parts have appeared, create an alert loser
+// // If all body parts have appeared, create an alert loser
+// ,
+// }
