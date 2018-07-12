ef7706759f593c90c87d7cebd99057170e4246c2
“Erin <erinkelley27@gmail.com>
2018-07-12 09:44:06 -0400

    word input field and array creation working

--- main.js
+++ main.js
@@ -1,20 +1,22 @@
 // Submit a word
 let submitWordButton = document.querySelector('#submit-word-button')
-let word = document.querySelector('#word-input-field').value
-
-// Split the string into an array
-let wordArray = word.split('')
+let word = document.querySelector('#word-input-field')
 
 // Create blanks array
 let blanksArray = []
 
 // Create blank spaces in the blanks - quantity to equal length of the word array
 submitWordButton.addEventListener('click', function (evt) {
-  evt.preventDefault()
-  for (let i = 0; i <= wordArray.length; i++) {
+  // Split the string into an array
+  let wordArray = word.value.split('')
+  for (let i = 0; i < wordArray.length; i++) { 
     blanksArray.push('_')
+    evt.preventDefault()
+    console.log(word.value)
+    console.log(wordArray)
+    console.log(blanksArray)
   }
-  // Add the blanks spaces into the blankSpaces div
+// Add the blanks spaces into the blankSpaces div
 //   let blanks = document.createElement('div')
 //   blanksArray = getElementsByClassName('blanksArray')
 //   blanksArray[0].blank - spaces.appendChild(blanks)
@@ -22,8 +24,8 @@ submitWordButton.addEventListener('click', function (evt) {
 
 //   for (let i = 0; i < blanksArray.length; i++) {
 //     blanks.appendChild(blanksArray[0])
-  }
-})
+//   }
+// })
 
 // Submit a guess letter
 // let submitLetterButton = document.querySelector('#submit-letter-button')
